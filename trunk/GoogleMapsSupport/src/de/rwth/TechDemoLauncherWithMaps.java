1fe9fb32d5c429378908e32c3d12305ade53ec04
simon.heinen <simon.heinen@4b3e571d-e50a-8ff4-66f0-eba65da14496>
2011-04-09 10:59:37 +0000

    Initial import.
    This additional project has to be used if the Google Maps view should be displayed!

---
+++ trunk/GoogleMapsSupport/src/de/rwth/TechDemoLauncherWithMaps.java
@@ -0,0 +1,91 @@
+package de.rwth;
+
+import system.ArActivity;
+import system.ErrorHandler;
+import system.Setup;
+import tests.SimpleTesting;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import commands.ui.CommandShowToast;
+
+import de.rwth.setups.ARNavigatorSetup;
+import de.rwth.setups.CollectItemsSetup;
+import de.rwth.setups.DebugSetup;
+import de.rwth.setups.IndoorSetup;
+import de.rwth.setups.PlaceObjectsSetup;
+import de.rwth.setups.PositionTestsSetup;
+import de.rwth.setups.SensorTestSetup;
+
+public class TechDemoLauncherWithMaps extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.demoselector);
+
+		// showSetup("With calibration dialogs", new CalibratorSetup());
+
+		showSetup("Animation Demo", new DebugSetup());
+		showSetup("Collecting Items Demo", new CollectItemsSetup());
+		showSetup("Placing objects Demo", new PlaceObjectsSetup());
+		showSetup("AR Navigator", new ARNavigatorSetup());
+		showSetup("Sensor Processing Demo", new SensorTestSetup());
+		showSetup("Position tests", new PositionTestsSetup());
+
+		LinearLayout l = ((LinearLayout) findViewById(R.id.demoScreenLinView));
+
+		l.addView(new SimpleButton("Run tests") {
+			public void onButtonPressed() {
+				runTests();
+			}
+		});
+
+		showSetup("Indoor Navigator (Needs special localization service!)",
+				new IndoorSetup());
+
+	}
+
+	private void showSetup(String string, final Setup aSetupInstance) {
+		((LinearLayout) findViewById(R.id.demoScreenLinView))
+				.addView(new SimpleButton(string) {
+					public void onButtonPressed() {
+						Activity theCurrentActivity = TechDemoLauncherWithMaps.this;
+						ARActivityPlusMaps.startWithSetup(theCurrentActivity,
+								aSetupInstance);
+					}
+				});
+	}
+
+	private abstract class SimpleButton extends Button {
+		public SimpleButton(String text) {
+			super(TechDemoLauncherWithMaps.this);
+			setText(text);
+			setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					onButtonPressed();
+				}
+			});
+		}
+
+		public abstract void onButtonPressed();
+	}
+
+	private void runTests() {
+		// execute all tests defined in the ARTestSuite:
+		try {
+			SimpleTesting.runAllTests(this);
+			new CommandShowToast(this, "All tests succeded on this device :)")
+					.execute();
+		} catch (Exception e) {
+			ErrorHandler.showErrorLog(this, e, true);
+		}
+	}
+
+}
---
+++ trunk/GoogleMapsSupport/src/geo/GMap.java
@@ -0,0 +1,247 @@
+package geo;
+
+import gui.CustomGestureListener;
+import system.EventManager;
+import system.TouchEventInterface;
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import commands.Command;
+
+/**
+ * To get a maps api key you have to use a keytool to generate a MD5 fingerprint
+ * for your computer. <br>
+ * <br>
+ * The keytool.exe is part of the Java SDK you are using (eg: C:\Program
+ * Files\Java\jdk1.6.0_22\bin\keytool.exe). <br>
+ * <br>
+ * So move the debug.keystore to C, start your cmd.exe and type: <br>
+ * <br>
+ * C:\Program Files\Java\jdk1.6.0_22\bin>keytool -list -alias androiddebugkey
+ * -keystore C:\debug.keystore -keypass android <br>
+ * <br>
+ * Here are more infos how to use the debug fingerprint:
+ * http://code.google.com/android/add-ons/google-apis/mapkey.html#
+ * getdebugfingerprint <br>
+ * <br>
+ * Now use the MD5 fingerprint you will get here:
+ * http://code.google.com/android/maps-api-signup.html <br>
+ * <br>
+ * The code you get from the signup page has to be passed in the constructor of
+ * the GMap class
+ * 
+ * @author Spobo
+ * 
+ */
+public class GMap extends MapView implements TouchEventInterface {
+
+	private static final String LOG_TAG = "GMap";
+
+	private Handler myHandler = new Handler();
+
+	// public LinearLayout myBox;
+
+	private GestureDetector myGestureDetector;
+	private Command myOnDoubleTabCommand;
+	private Command myOnLongPressCommand;
+	private Command myOnTabCommand;
+
+	private GeoUtils myGeoUtils;
+
+	// private MotionEvent currentEvent;
+
+	public GMap(Context c, String apiKey) {
+		this(c, apiKey, false);
+	}
+
+	/**
+	 * Same method as setBuiltInZoomControls(), only purpose is to better find
+	 * this method
+	 * 
+	 * @param showThem
+	 */
+	public void enableZoomButtons(boolean showThem) {
+		this.setBuiltInZoomControls(showThem);
+	}
+
+	/**
+	 * @param box
+	 *            the {@link MapView} will fill the complete box
+	 * @param apiKey
+	 *            see {@link GMap}
+	 * @param longPressEnanabled
+	 *            when long pressing is enabled, the default map movemant won't
+	 *            work, so default if false
+	 */
+	public GMap(Context c, String apiKey, boolean longPressEnanabled) {
+		super(c, apiKey);
+
+		setLayoutParams(new android.view.ViewGroup.LayoutParams(
+				LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+
+		myGeoUtils = new GeoUtils(c);
+
+		// create a GestureDetector:
+		myGestureDetector = new GestureDetector(c, new CustomGestureListener(
+				this));
+		myGestureDetector.setIsLongpressEnabled(longPressEnanabled);
+	}
+
+	public boolean setCenterTo(GeoObj myGeoObj) {
+
+		try {
+			this.getController().animateTo(GMap.toGeoPoint(myGeoObj));
+			this.invalidate();
+			return true;
+		} catch (Exception e) {
+			Log.e(LOG_TAG, "Probably no internet connection available");
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+	public void addOverlay(final Overlay mapOverlay) {
+		// update map through a handler (event might come not from from main
+		// thread):
+		final GMap map = this;
+		myHandler.post(new Runnable() {
+			public void run() {
+				Log.d(LOG_TAG, "Adding map overlay: " + mapOverlay);
+				map.getOverlays().add(mapOverlay);
+				try {
+					map.invalidate();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public void setCenterToCurrentPos() {
+		setCenterTo(EventManager.getInstance().getCurrentLocationObject());
+	}
+
+	/**
+	 * @param level
+	 *            1 is whole world, 21 is closest zoom level
+	 */
+	public void setZoomLevel(int level) {
+		this.getController().setZoom(level);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		/*
+		 * to understand the idea of redirecting the events look at the
+		 * MyGestureListener class which will inform the map when a special
+		 * event like a tab, doubleTab, LongPress, .. happened
+		 */
+		myGestureDetector.onTouchEvent(event);
+		return super.onTouchEvent(event);
+	}
+
+	@Override
+	public void onDoubleTap(MotionEvent e) {
+		Command c = getOnDoubleTabCommand();
+		if (c != null) {
+			GeoObj p = newGeoObj(getProjection().fromPixels((int) e.getX(),
+					(int) e.getY()));
+			c.execute(p);
+		}
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		Command c = getOnLongPressCommand();
+		if (c != null) {
+			GeoObj p = newGeoObj(getProjection().fromPixels((int) e.getX(),
+					(int) e.getY()));
+			c.execute(p);
+		}
+	}
+
+	@Override
+	public void onSingleTab(MotionEvent e) {
+		Command c = getOnTabCommand();
+		if (c != null) {
+			GeoObj p = newGeoObj(getProjection().fromPixels((int) e.getX(),
+					(int) e.getY()));
+			c.execute(p);
+		}
+	}
+
+	private GeoObj newGeoObj(GeoPoint gp) {
+		return new GeoObj(gp.getLatitudeE6() / 1E6, gp.getLongitudeE6() / 1E6);
+	}
+
+	@Override
+	public Command getOnDoubleTabCommand() {
+		return myOnDoubleTabCommand;
+	}
+
+	@Override
+	public Command getOnLongPressCommand() {
+		return myOnLongPressCommand;
+	}
+
+	@Override
+	public Command getOnTabCommand() {
+		return myOnTabCommand;
+	}
+
+	@Override
+	public void setOnDoubleTabCommand(Command c) {
+		myOnDoubleTabCommand = c;
+	}
+
+	@Override
+	public void setOnLongPressCommand(Command c) {
+		myOnLongPressCommand = c;
+	}
+
+	@Override
+	public void setOnTabCommand(Command c) {
+		setOnTabCommand(c, true);
+	}
+
+	/**
+	 * this is the command that will be executed if the {@link GMap} is tabed.
+	 * if the DefaultOnClickCommand for the map isn't set yet this will be done
+	 * too, so if there will be items on the map and those items are selected
+	 * the OnMapTouchReleasedCommand will be executed too
+	 */
+	public void setOnTabCommand(Command c,
+			boolean setDefaultItemSelectCommandToo) {
+
+		// if (setDefaultItemSelectCommandToo
+		// && getOnTabCommand() == null)
+		// setDefaultOnClickCommand(c);
+		myOnTabCommand = c;
+	}
+
+	@Override
+	public void onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// do nothing, because scrolling already done by the map
+	}
+
+	@Deprecated
+	public GeoUtils getGeoUtils() {
+		return myGeoUtils;
+	}
+
+	public static GeoPoint toGeoPoint(GeoObj x) {
+		return new GeoPoint((int) (x.getLatitude() * 1E6),
+				(int) (x.getLongitude() * 1E6));
+	}
+
+}
