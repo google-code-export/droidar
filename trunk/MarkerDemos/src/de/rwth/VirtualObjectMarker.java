d872b97875a8ef0b16d0fd8e4bf3c5dc609c67fa
simon.heinen <simon.heinen@4b3e571d-e50a-8ff4-66f0-eba65da14496>
2011-05-28 10:42:30 +0000

    now the camera position should be taken into account correctly

--- trunk/MarkerDemos/src/de/rwth/VirtualObjectMarker.java
+++ trunk/MarkerDemos/src/de/rwth/VirtualObjectMarker.java
@@ -11,7 +11,8 @@ public class VirtualObjectMarker implements MarkerObject {
 
 	private MeshComponent myTargetMesh;
 	private GLCamera myCamera;
-	//private float[] viewMatrix = null;
+
+	// private float[] viewMatrix = null;
 
 	public VirtualObjectMarker(MeshComponent m, GLCamera camera) {
 		myTargetMesh = m;
@@ -26,21 +27,23 @@ public class VirtualObjectMarker implements MarkerObject {
 	@Override
 	public void OnMarkerPositionRecognized(float[] rotMatrix, int start,
 			int end, int sideAngle) {
-		Log.d("AR", "matrix length "+ rotMatrix.length);
-		Log.d("AR", "matrix end "+ end);
-		Log.d("AR", "matrix start "+ start);
-		
-		
-		//viewMatrix = myCamera.getRotationMatrix();
-		//Matrix.multiplyMM(modelViewMatrix, 0, myCamera.getRotationMatrix(),
-			//	myCamera.getMatrixOffset(), GLCamera.createIdentityMatrix(), 0);//rotMatrix, start);
-		float[] centerVec = { rotMatrix[start+12], rotMatrix[start+13], rotMatrix[start+14], 1 };
+		Log.d("AR", "matrix length " + rotMatrix.length);
+		Log.d("AR", "matrix end " + end);
+		Log.d("AR", "matrix start " + start);
+
+		// viewMatrix = myCamera.getRotationMatrix();
+		// Matrix.multiplyMM(modelViewMatrix, 0, myCamera.getRotationMatrix(),
+		// myCamera.getMatrixOffset(), GLCamera.createIdentityMatrix(),
+		// 0);//rotMatrix, start);
+		float[] centerVec = { rotMatrix[start + 12], rotMatrix[start + 13],
+				rotMatrix[start + 14], 1 };
 		float[] resultVec = { 0, 0, 0, 1 };
 		float[] invViewMatrix = new float[16];
-		Matrix.invertM(invViewMatrix , 0, myCamera.getRotationMatrix(), 0);
+		Matrix.invertM(invViewMatrix, 0, myCamera.getRotationMatrix(), 0);
 		Matrix.multiplyMV(resultVec, 0, invViewMatrix, 0, centerVec, 0);
-		myTargetMesh.myPosition = new Vec(resultVec[0], resultVec[1],
-				resultVec[2]);
+		Vec camPos = myCamera.getMyPosition();
+		myTargetMesh.myPosition = new Vec(resultVec[0] + camPos.x, resultVec[1]
+				+ camPos.y, resultVec[2] + camPos.z);
 		System.out.println(myTargetMesh.myPosition);
 	}
 
